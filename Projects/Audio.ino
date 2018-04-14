105f515f1d5910ea25b2bb7be9860961208be031
Ben Fairlamb <25651348+BrokenScience@users.noreply.github.com>
2018-04-14 15:35:25 -0400

    Audio Code

---
+++ Projects/Audio.ino
@@ -0,0 +1,48 @@
+#include <toneAC.h>
+
+int uber = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+    int out;
+    out = analogRead(A0);
+    out -= 512;
+    out = abs(out);
+    Serial.println(out);
+    if (out > 250)
+    {
+      if (out < 300)
+      {
+        toneAC(out % 10 * 500, 2);
+        //Serial.println(out % 10 * 500);
+      }
+      else if (out < 350)
+      {
+        toneAC(out % 10 * 500, 4);
+        //Serial.println(out % 10 * 500);
+      }
+      else if (out < 400)
+      {
+        toneAC(out % 10 * 500, 6);
+        //Serial.println(out % 10 * 500);
+      }
+      else if (out < 450)
+      {
+        toneAC(out % 10 * 500, 8);
+        //Serial.println(out % 10 * 500);
+      }
+      else
+      {
+        toneAC(out % 10 * 500, 10);
+        //Serial.println(out % 10 * 500);
+      }
+   }
+   else
+   {
+    toneAC();
+   }
+}
