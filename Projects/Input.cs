c49ee7f6d9856b97d216ec34d0ed18c4516b66e2
Ben Fairlamb <25651348+BrokenScience@users.noreply.github.com>
2018-05-05 18:56:07 -0400

    Add files via upload

---
+++ Projects/Input.cs
@@ -0,0 +1,60 @@
+﻿using System; //So we catch exceptions & ignore them
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports; //So we can do serial
+
+public class Input : MonoBehaviour
+{
+
+    public string aPortName;  //Set in the Inspector after checking
+    public int baud = 9600;
+    SerialPort stream;
+
+    [SerializeField]
+    private Hole[] holes; // Set in inspector
+
+    // Use this for initialization
+    void Start()
+    {
+        stream = new SerialPort(aPortName, baud);
+        stream.ReadTimeout = 500;
+        stream.Open(); //Open serial stream
+        //Behavior expected from Arduino's SerialCallResponseASCII sketch
+        stream.Write("1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        try
+        {
+            string value = stream.ReadLine();
+            Debug.Log(value);
+            if (value == "0")
+            {
+            }
+            else
+            {
+                char[] buttons = value.ToCharArray();
+                for (int i = 0; i < 6; i++)
+                {
+                    if (buttons[i] == '1')
+                    {
+                        holes[i].Hitting = true;
+                    }
+                    else
+                    {
+                        holes[i].Hitting = false;
+                    }
+                }
+            }
+            stream.Write("1"); //tell arduino to keep 
+        }
+        catch (Exception e)
+        {
+            //no op
+        }
+
+    }
+}
---
+++ Projects/Serial/Serial.ino
@@ -0,0 +1,34 @@
+void setup() {
+  // start serial port at 9600 bps and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // Set button pins
+  for (int i = 2; i <= 7; i++)
+  {
+    pinMode(i, INPUT);
+  }
+  establishContact();  // send a byte to establish contact until receiver responds
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    for (int i = 2; i <= 7; i++)
+    {
+      Serial.print(digitalRead(i));
+    }
+    Serial.println();
+    
+    // wait for character to continue
+    Serial.read();
+  }
+}
+
+void establishContact() {
+  Serial.println("0");   // send an initial string
+  while (Serial.available() <= 0) {
+    delay(300);
+  }
+}
